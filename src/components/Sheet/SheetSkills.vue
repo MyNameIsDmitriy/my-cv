5551e096a0f07363b317257d842f22c9e7a5c7a4
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2025-01-13 17:47:55 +0300

    feat: add skills section

---
+++ src/components/Sheet/SheetSkills.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex flex-col gap-4 mt-4">
+    <h3 class="text-[13px] font-bold font-plex tracking-[0.2px]">Skills</h3>
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-1.5 text-[11px]">
+        <h4 class="leading-4 font-medium font-plex">Programming Languages</h4>
+        <p class="leading-5 opacity-60">JavaScript(ES6+), TypeScript</p>
+      </div>
+      <div class="flex flex-col gap-1.5 text-[11px]">
+        <h4 class="leading-4 font-medium font-plex">Frontend Stack</h4>
+        <p class="leading-5 opacity-60">
+          Vue.js, Nuxt, Vuex, Pinia, Vue Router, Tailwind CSS, Webpack, Vite, Babel, HTML5, CSS3,
+          SCSS
+        </p>
+      </div>
+      <div class="flex flex-col gap-1.5 text-[11px]">
+        <h4 class="leading-4 font-medium font-plex">Backend Stack</h4>
+        <p class="leading-5 opacity-60">Node.js, Express, PostgreSQL, MySQL, Sequelize, REST</p>
+      </div>
+      <div class="flex flex-col gap-1.5 text-[11px]">
+        <h4 class="leading-4 font-medium font-plex">DevOps Stack</h4>
+        <p class="leading-5 opacity-60">Docker, Postman, Git(GitHub), Swagger</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/TheSheet.vue
+++ src/components/TheSheet.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import SheetHeader from '@/components/Sheet/SheetHeader.vue'
+import SheetSkills from '@/components/Sheet/SheetSkills.vue'
 </script>
 
 <template>
   <div class="w-[650px] h-[877px] pt-10 pb-5 pl-16 pr-5 bg-white">
     <SheetHeader />
+    <SheetSkills />
   </div>
 </template>
